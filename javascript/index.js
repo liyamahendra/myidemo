d086529bda0fc7249926bfba4e4997cb48ffe75e
Mahendra Liya <liyamahendra4@gmail.com>
2012-07-20 15:07:22 +0530

    adding the navigation flow

---
+++ javascript/index.js
@@ -0,0 +1,47 @@
+var screenStack = [];
+//------------------------------------------------------------------
+function init(){
+	window.addEventListener("deviceready", onDeviceReady, true);
+}
+$(document).ready(function() {
+  $.ajaxSetup({ cache: false });
+});
+function onDeviceReady()
+{
+
+}
+
+function nullHandler() {}
+//------------------------------------------------------------------
+
+function setSearchForValue(listIndex)
+{
+	if(listIndex == 1)
+	{
+		document.getElementById("searchFor").value = "attorney";
+	}
+	else if(listIndex == 2)
+	{
+		document.getElementById("searchFor").value = "judge";		
+	}
+	
+	$("#homeScreen").hide();
+	$("#optionsScreen").show("slide", { direction: "right" }, 250);
+	
+	screenStack.push("homeScreen");
+}
+
+function goBack(currentScreen)
+{
+	if(screenStack.length > 0)
+	{
+		var currentScreenObject = document.getElementById(currentScreen);
+		currentScreenObject.style.display = "none";
+
+		var previousScreen = screenStack.pop();
+		var previousScreenObject = document.getElementById(previousScreen);		
+		//previousScreenObject.style.display = "block";
+		$(previousScreenObject).show("slide", { direction: "left" }, 250);
+		
+	}
+}
